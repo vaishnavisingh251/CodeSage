6b774e81bda6dade680439e43722057a6f05ef19
Vaishnavi Singh <vaishsingh9696@gmail.com>
2026-04-29 08:06:12 +0530

    fix correct backend url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     setIsLoading(true)
     setReview("")
     try {
-      const response = await axios.post('https://codesage-hjeo.onrender.com', {
+      await axios.post('https://codesage-hjeo.onrender.com/ai/get-review', {
         code,
         language
       })
